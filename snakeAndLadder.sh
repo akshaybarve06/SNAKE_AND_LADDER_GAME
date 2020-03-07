25be05805232c76ec5f0ea1a9e78d917acabcc1d
Akshay Barve <61134490+akshaybarve06@users.noreply.github.com>
2020-03-07 22:19:11 +0530

    Added: Playing Two Players In Game

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -7,53 +7,103 @@
 
 startPosition=0
 endPosition=100
-position=0
+playerOnePosition=0
+playerTwoPosition=0
 diceRoll=1
+diceOutput=0
+turn=0
 
 function play()
 {
-while [[ $position -lt $endPosition ]]
+while [[ $playerTwoPosition -ne $endPosition && $playerOnePosition -ne $endPosition ]]
 do
+	((turn++))
 	diceOutput=$((RANDOM%6+1))
 	choice=$((RANDOM%3))
 
-	case $choice in
-		0)
-			#No Play Condition
-			if [[ $position -lt 0 ]]
-  			then
-				position=$startPosition
-			else
-      		position=$(($position+0))
-			fi
-			;;
-		1)
-			#Snake
-			if [[ $position -lt 0 ]]
-			then
-				position=$startPosition
-			else
-				position=$(($position-$diceOutput))
-			fi
-			;;
-		2)
-			#Ladder
-			if [[ $(($endPosition - $position)) -ge $diceOutput ]]
-			then
-				position=$(($position+$diceOutput))
-			else
-				position=$(($position+0))
-			fi
+		if [[ $choice -eq 0 ]]
+		then
+			noPlay
+		elif [[ $choice -eq 1 ]]
+		then
+			snake
+		elif [[ $choice -eq 2 ]]
+		then
+			ladder
+		fi
 
-			;;
-		*)
-			echo Invalid
-			;;
-	esac
 	((diceRoll++))
-	echo Position After $diceRoll th Roll.. $position
 done
-echo Player Won
-}
-play
+      if [[ $playerOnePosition  -eq 100 ]]
+      then
+         echo Player 1 Won
+      elif [[ $playerTwoPosition -eq 100 ]]
+      then
+         echo Player 2 Won
+      fi
 echo Number of Times Dice Roll..$diceRoll
+}
+
+
+
+function noPlay()
+{
+if [[ $(($turn % 2)) -eq 1 ]]
+then
+	if [[ $playerOnePosition -lt 0 ]]
+	then
+		playerOnePosition=$startPosition
+	else
+ 		playerOnePosition=$(($playerOnePosition+0))
+	fi
+else
+	if [[ $playerTwoPosition -lt 0 ]]
+	then
+		playerTwoPosition=$startPosition
+	else
+ 		playerTwoPosition=$(($playerTwoPosition+0))
+	fi
+fi
+}
+
+function snake()
+{
+if [[ $(($turn % 2)) -eq 1 ]]
+then
+	if [[ $playerOnePosition -lt 0 ]]
+	then
+		playerOnePosition=$startPosition
+	else
+		playerOnePosition=$(($playerOnePosition-$diceOutput))
+	fi
+else
+	if [[ $playerTwoPosition -lt 0 ]]
+	then
+		playerTwoPosition=$startPosition
+	else
+		playerTwoPosition=$(($playerTwoPosition-$diceOutput))
+	fi
+fi
+}
+
+function ladder()
+{
+if [[ $(($turn % 2)) -eq 1 ]]
+then
+	if [[ $(($endPosition - $playerOnePosition)) -ge $diceOutput ]]
+	then
+		playerOnePosition=$(($playerOnePosition+$diceOutput))
+	else
+		playerOnePosition=$(($playerOnePosition+0))
+	fi
+else
+	if [[ $(($endPosition - $playerTwoPosition)) -ge $diceOutput ]]
+	then
+		playerTwoPosition=$(($playerTwoPosition+$diceOutput))
+	else
+		playerTwoPosition=$(($playerTwoPosition+0))
+	fi
+fi
+}
+
+play
