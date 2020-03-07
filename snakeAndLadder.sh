66b1d5139be882b9cd34a6cbfe249be079bd8a76
Admin <none@gmail.com>
2020-03-07 17:20:13 +0530

    Added: Game Start With Position Zero

---
+++ snakeAndLadder.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+# @ Description: Snake And Ladder Problem
+# @ Author: Akshay Dhananjay Barve
+# @ Version: 18.04.3 lts
+# @ Since: 07 March 2020 / Saturday
+
+startPosition=0
+endPosition=100
