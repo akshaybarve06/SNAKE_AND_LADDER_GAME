5fafb35f630b751b0c7104ed1a7d32a91debe4f1
Admin <none@gmail.com>
2020-03-07 17:39:57 +0530

    Added: Check Randomly is Snake Ladder or NoPlay

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -7,6 +7,21 @@
 
 startPosition=0
 endPosition=100
+position=0
 
-diceOutput=$((RANDOM%6+1))
-
+	diceOutput=$((RANDOM%6+1))
+	choice=$((RANDOM%3))
+	case $choice in
+		0)
+			# No Play Condition
+			position=$(($position+0))
+			;;
+		1)
+			# Snake
+			position=$(($position-$diceOutput))
+			;;
+		*)
+			# ladder
+			position=$(($position+$diceOutput))
+			;;
+	esac
